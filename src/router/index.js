dfd9eaba4a6334320b4bd134f0d558adca5f4ce3
Raúl Domínguez <rauldominguez@Rauls-MacBook-Air.local>
2024-07-30 16:19:45 -0600

    [ADD] moving to this repository

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import privateRoutes from "@/router/private-routes";
+import publicRoutes from "@/router/public-routes";
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path:'',
+        redirect:'/login'
+    },
+    {
+        path:'/',
+        component: {
+            render(c) { return c('router-view') }
+        },
+        children: [
+            ...privateRoutes.map(route => {
+                route.meta.requiresAuth = false;
+                return {...route};
+            }),
+            ...publicRoutes.map(route => {
+                route.meta.requiresAuth = false;
+                return {...route};
+            })
+
+        ]
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
